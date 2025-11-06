a3e863b05bf0ee4146fd1f06a8a4b4675d57651b
Ajit Sakhare <45509860+AjitSakhare@users.noreply.github.com>
2025-11-06 21:29:47 +0530

    Add files via upload
    Perform Basic Mathematical Operations

---
+++ Perform Basic Mathematical Operations.py
@@ -0,0 +1,13 @@
+first_number = int(input("Enter the first number: "))    #Take the input from user and convert it to int. We can take float as input as well
+second_number = int(input("Enter the second number: "))  #Take the second input from user similar to first one.
+
+Addition = first_number + second_number  #Addition of two numbers
+Subtraction = first_number - second_number #Substraction of two numbers
+Multiplication = first_number * second_number #Multiplication of two numbers
+Division = first_number / second_number #Division of two numbers
+
+#Below is the display of all operations
+print("Addition: ", Addition)
+print("Subtraction: ", Subtraction)
+print("Multiplication: ", Multiplication)
+print("Division: ",Division)
