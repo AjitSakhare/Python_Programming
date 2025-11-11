60c4229aeaeb8972ecdf2c75898230c7fae3313c
Ajit Sakhare <tech.ajitsakhare@gmail.com>
2025-11-11 20:21:53 +0530

    Update DiceRollSimulation.py

--- DiceRollSimulation.py
+++ DiceRollSimulation.py
@@ -9,10 +9,13 @@ print("Welcome to game of rolling dice.")
 
 while True:
     choice=input("Press enter to roll a dice ")
+    choice=choice.strip()
     if choice=='q':
         print("Thank you for playing a game. Bye!")
+        break
     elif choice=='':
         number=random.randint(1,6)
         print(f"Your number is {number}")
     else:
+
         print("Invalid Number!!")
