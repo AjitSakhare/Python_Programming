87dfa3f4cd2e17a6d109d7177d6f3a2cf3fe00ce
Ajit Sakhare <tech.ajitsakhare@gmail.com>
2025-11-11 12:46:54 +0530

    Add files via upload

---
+++ string operations in python.py
@@ -0,0 +1,41 @@
+# """slicing of string in python """
+#
+# string_input = "I am happy to learn python"
+#
+# print(len(string_input))
+# print(string_input[5])
+#
+# string_slicing= string_input[2:5]
+# print(string_slicing)
+#
+# string_slicing= string_input[2:6:1]
+# print(string_slicing)
+#
+# string_slicing= string_input[1+1:9-2:1*1]
+# print(string_slicing)
+#
+# """ fstring in python - formating the string."""
+#
+# first_name ="Ajit"
+# last_name="Sakhare"
+# course ="Python"
+# marks=80
+#
+# print(first_name,"has took",course,"course online and completed with",marks,"% marks")
+# print(f"{first_name} has took {course} course online and completed with {marks} % marks")
+# ajit_details=f"{first_name} has took {course} course online and completed with {marks} % marks"
+# print(ajit_details)
+#
+# """Escape sequence in python"""
+#
+# print("it's ok sometimes things not go well")
+# print('it\'s ok sometimes things not go well')
+# print("it\"s ok sometimes things not go well")
+# print('it"s ok sometimes things not go well')
+# print('its ok sometimes\nthings not go\twell')
+# print('it\'s ok sometimes things go good\\bad in life')
+
+#Operations in python
+#print("ajit"+"sakhare")  #string concatination with "+" operator
+# print("ajit" * "sakhare") this not works
+# print("ajit" * 3) # output - ajitajitajit its repeatative of world or string.
