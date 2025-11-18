02bc903a167129e2acc9965e1fbf321adb4bd26e
Ajit Sakhare <tech.ajitsakhare@gmail.com>
2025-11-18 21:24:03 +0530

    Add files via upload

---
+++ DemonstrateListSlicing.py
@@ -0,0 +1,8 @@
+num_list=[1,2,3,4,5,6,7,8,9,10]
+print(f"Original list:- {num_list}")
+
+first_five = num_list[0:5]
+reversed_first_five = first_five[::-1]
+
+print(f"Extracted first five elements:- {first_five}")
+print(f"Extracted first five elements:- {reversed_first_five}")
